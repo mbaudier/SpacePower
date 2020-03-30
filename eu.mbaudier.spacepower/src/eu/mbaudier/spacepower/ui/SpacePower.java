e7367547d400f4b315f43e47ae115419231c420b
Mathieu Baudier <mbaudier@argeo.org>
2020-03-30 17:39:31 +0200

    Space ship moving with keyboard arrows.

--- eu.mbaudier.spacepower/src/eu/mbaudier/spacepower/ui/SpacePower.java
+++ eu.mbaudier.spacepower/src/eu/mbaudier/spacepower/ui/SpacePower.java
@@ -1,16 +1,74 @@
 package eu.mbaudier.spacepower.ui;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 /** Main class assembling the software. */
-public class SpacePower {
+public class SpacePower implements KeyListener {
+	private Canvas canvas;
+	private Image spaceShipImage;
+
+	private int spaceShipY = 0;
+	private int spaceShipYmove = 50;
+
+	public SpacePower(Shell shell) {
+		Display display = shell.getDisplay();
+		canvas = new Canvas(shell, SWT.NONE);
+		spaceShipImage = new Image(display, "images/SpacePower-Spaceship.png");
+
+		canvas.addListener(SWT.Paint, e -> {
+			GC gc = e.gc;
+			paintScreen(gc);
+		});
+
+		canvas.addKeyListener(this);
+	}
+
+	public void paintScreen(GC gc) {
+		int canvasHeight = canvas.getSize().y;
+
+		// Space ship
+		// in case canvas has been resized we reset space ship Y
+		int imageHeight = spaceShipImage.getBounds().height;
+		if (spaceShipY >= (canvasHeight - imageHeight))
+			spaceShipY = canvasHeight - imageHeight;
+		gc.drawImage(spaceShipImage, 0, spaceShipY);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// System.out.println(e);
+		if (e.keyCode == 0x1000001) {// up
+			if (spaceShipY <= 0)
+				return;
+			spaceShipY = spaceShipY - spaceShipYmove;
+		} else if (e.keyCode == 0x1000002) {// down
+			int canvasHeight = canvas.getSize().y;
+			int imageHeight = spaceShipImage.getBounds().height;
+			if (spaceShipY >= (canvasHeight - imageHeight - spaceShipYmove))
+				return;
+			spaceShipY = spaceShipY + spaceShipYmove;
+		}
+		System.out.println("spaceShipY=" + spaceShipY);
+
+		// repaint canvas
+		canvas.redraw();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public void dispose() {
+		spaceShipImage.dispose();
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to SpacePower!!");
@@ -19,16 +77,7 @@ public class SpacePower {
 		Shell shell = new Shell(display);
 		shell.setText("SpacePower");
 		shell.setLayout(new FillLayout());
-		Image originalImage = null;
-		originalImage = new Image(display, "images/SpacePower-Spaceship.png");
-		final Image image = originalImage;
-		final Point origin = new Point(0, 200);
-		final Canvas canvas = new Canvas(shell, SWT.NONE);
-
-		canvas.addListener(SWT.Paint, e -> {
-			GC gc = e.gc;
-			gc.drawImage(image, origin.x, origin.y);
-		});
+		SpacePower spacePower = new SpacePower(shell);
 
 		shell.setSize(800, 600);
 		shell.open();
@@ -36,7 +85,7 @@ public class SpacePower {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
-		originalImage.dispose();
+		spacePower.dispose();
 		display.dispose();
 
 	}
